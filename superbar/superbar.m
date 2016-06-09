b919c93a57dacc0f86cdff70bb668560bad6f55b
Scott Lowe <scott.code.lowe@gmail.com>
2016-06-09 22:32:19 +0100

    MAINT: Replot vertical comparison lines after text
    This prevents the background for the text being on top of the
    horizontal lines. We only want the text background to take precedence
    over the vertical lines.

--- superbar/superbar.m
+++ superbar/superbar.m
@@ -744,7 +744,7 @@ current_height = repmat(YEO(:)', N, 1) + offset / 2;
 
 % Loop over every pair with a measurement
 num_comparisons = sum(~isnan(P(:)));
-hl = nan(num_comparisons, 1);
+hl = nan(num_comparisons, 2);
 ht = nan(num_comparisons, 1);
 hbl = nan(num_comparisons, 1);
 coords = nan(4, 2, num_comparisons);
@@ -806,7 +806,7 @@ for iPair=num_comparisons:-1:1
     if pad_lines
         hbl(iPair) = line(xx, yy, pad_args{:});
     end
-    hl(iPair) = line(xx, yy, line_args{:});
+    hl(iPair, 1) = line(xx, yy, line_args{:});
     % Check how many stars to put in the text
     num_stars = sum(P(ISi(iPair), ISj(iPair)) <= p_threshold);
     str = repmat('*', 1, num_stars);
@@ -830,6 +830,21 @@ for iPair=num_comparisons:-1:1
         'Rotation', rotation, ...
         'Interpreter', 'latex', ...
         text_args{:});
+end
+
+% Plot the horizontal lines again, on top of everything else (specifically
+% so they are on top of the text)
+for iPair=num_comparisons:-1:1
+    % Get co-ordinates back again
+    if strncmpi(orientation, 'h', 1)
+        xx = coords(:, 2, iPair);
+        yy = coords(:, 1, iPair);
+    else
+        xx = coords(:, 1, iPair);
+        yy = coords(:, 2, iPair);
+    end
+    % Draw the horizontal lines again on top of everything else
+    hl(iPair, 2) = line(xx(2:3), yy(2:3), line_args{:});
 end
 
 end
